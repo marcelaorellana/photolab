a0ae845d43fc34d7826530f18735d30347eb26e9
marcelaorellana <marcelapaz.1985@gmail.com>
2017-07-28 09:08:44 -0400

    Trabajo terminado

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    var scroll_start = 0;
+    var startchange = $('nav');
+    var offset = startchange.offset();
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $('nav').css('background-color', 'white');
+          $('.nav-wrapper ul li a').css('color', 'gray');
+       } else {
+          $('nav').css('background-color', 'transparent');
+          $('.nav-wrapper ul li a').css('color', 'white');
+       }
+   });
+	$(".button-collapse").sideNav();
+});
